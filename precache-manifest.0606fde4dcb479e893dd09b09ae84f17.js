b03797818d366e84ce3decb28fea9c18b47794dc
Alice Miao <Wu@D-10-19-105-217.dhcp4.washington.edu>
2018-11-15 14:25:13 -0800

    Updates

---
+++ precache-manifest.0606fde4dcb479e893dd09b09ae84f17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23f64c38bebfb3400133",
+    "url": "/cars-project/static/js/runtime~main.23f64c38.js"
+  },
+  {
+    "revision": "415c50c415a12a86f904",
+    "url": "/cars-project/static/js/main.415c50c4.chunk.js"
+  },
+  {
+    "revision": "1a92331d4f12c4b3f1f8",
+    "url": "/cars-project/static/js/1.1a92331d.chunk.js"
+  },
+  {
+    "revision": "415c50c415a12a86f904",
+    "url": "/cars-project/static/css/main.e7ccd7df.chunk.css"
+  },
+  {
+    "revision": "8424c6dd72039143f8632faa78b9e60c",
+    "url": "/cars-project/index.html"
+  }
+];
